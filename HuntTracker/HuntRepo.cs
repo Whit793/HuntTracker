67fa73710ff06ab887cc87c736a43dbf3f57438c
Bryan Whitman <BW.Hitman89@gmail.com>
2021-01-13 21:00:46 -0600

    Added Hunt Class

---
+++ HuntTracker/HuntRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using HuntTracker.Models;
+
+namespace HuntTracker
+{
+    public class HuntRepo : IHuntRepo
+    {
+        private readonly IDbConnection _conn;
+        public IEnumerable<Hunt> GetAllHunts()
+        {
+            throw new NotImplementedException();
+
+
+        }
+    }
+}
---
+++ HuntTracker/Models/Hunt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HuntTracker.Models
+{
+    public class Hunt
+    {
+    }
+}
